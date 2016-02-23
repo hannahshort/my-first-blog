38f3a92209e6662cb1c296d7b23252cae67cb103
hannahshort <hannah.short08@gmail.com>
2016-02-23 11:45:40 +0100

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,6 @@
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-#Maybe need to add this!
-from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
