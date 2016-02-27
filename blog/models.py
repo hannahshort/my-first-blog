c16d7e266b74f43062db8115c388e60c8b71f46a
hannahshort <hannah.short08@gmail.com>
2016-02-27 18:04:47 +0100

    adding a like button

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    likes = models.PositiveSmallIntegerField(blank=True, null=True, editable=False, default=0)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def like(self):
+        self.likes=self.likes+1;
--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.like()
+    post.save()
+    return redirect('post_detail', pk=post.pk)
